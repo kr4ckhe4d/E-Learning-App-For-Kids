b302e2bfb19db68ed6d5c878316d3c9766bb5623
Nipuna <nipuna@app-monkeyz.com>
2017-06-11 23:17:56 +0530

    add items provider

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,6 +11,7 @@ import { TabsPage } from '../pages/tabs/tabs';
 
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
+import { ItemsProvider } from '../providers/items/items';
 
 @NgModule({
   declarations: [
@@ -37,7 +38,8 @@ import { SplashScreen } from '@ionic-native/splash-screen';
   providers: [
     StatusBar,
     SplashScreen,
-    {provide: ErrorHandler, useClass: IonicErrorHandler}
+    {provide: ErrorHandler, useClass: IonicErrorHandler},
+    ItemsProvider
   ]
 })
 export class AppModule {}
--- src/pages/items/items.ts
+++ src/pages/items/items.ts
@@ -1,3 +1,4 @@
+import { ItemsProvider } from '../../providers/items/items';
 import { ItemPage } from '../item/item';
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
@@ -16,20 +17,8 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 export class ItemsPage {
   products = [];
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
-    this.products.push({
-      'src':'https://support.apple.com/content/dam/edam/applecare/images/en_US/iphone/featured-content-iphone-transfer-content-ios10_2x.png',
-      'description': 'Patta phone eka',
-      'name': 'iPhone 4S',
-      'price_discount': 55.0,
-      'price': 500
-  },{
-      'src':'https://store.storeimages.cdn-apple.com/4974/as-images.apple.com/is/image/AppleInc/aos/published/images/i/ph/iphone7/plus/iphone7-plus-black-select-2016?wid=1200&hei=630&fmt=jpeg&qlt=95&op_sharpen=0&resMode=bicub&op_usm=0.5,0.5,0,0&iccEmbed=0&layer=comp&.v=1472430090682',
-      'description': 'Patta phone eka',
-      'name': 'iPhone 4S',
-      'price_discount': 55.0,
-      'price': 500
-  });
+  constructor(public navCtrl: NavController, public navParams: NavParams, public itemsProvider:ItemsProvider) {
+    this.products = itemsProvider.getItems();
   }
 
   ionViewDidLoad() {
