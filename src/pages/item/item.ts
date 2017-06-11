22e6e1c3f7a982fda73dfe4b16742d82d36a0a1b
Nipuna <nipuna@app-monkeyz.com>
2017-06-11 23:27:43 +0530

    cart provider done

--- src/pages/item/item.ts
+++ src/pages/item/item.ts
@@ -1,12 +1,7 @@
+import { ItemsProvider } from '../../providers/items/items';
 import { Component } from '@angular/core';
 import { IonicPage, LoadingController, NavController, NavParams } from 'ionic-angular';
 
-/**
- * Generated class for the ItemPage page.
- *
- * See http://ionicframework.com/docs/components/#navigation for more info
- * on Ionic pages and navigation.
- */
 @IonicPage()
 @Component({
   selector: 'page-item',
@@ -15,7 +10,7 @@ import { IonicPage, LoadingController, NavController, NavParams } from 'ionic-an
 export class ItemPage {
 product:any;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams, public loadingCtrl: LoadingController) {
+  constructor(public navCtrl: NavController, public navParams: NavParams, public loadingCtrl: LoadingController, public itemsProvider: ItemsProvider) {
     this.product = this.navParams.get('product');
   }
 
@@ -24,44 +19,6 @@ product:any;
   }
 
   addToCart(product) {
-  	
-  	// let loading = this.loadingCtrl.create({
-	  //   content: 'Adding to cart...'
-	  // });
-  	// loading.present();
-
-    // let line_item:any = {};
-
-    // line_item.product_id = this.product.id;
-    // line_item.quantity = 1;
-
-    // if (this.product.type === 'product_with_variants'){
-    //   line_item.options = this.selectedVariants;
-    // }
-
-    // this.marketcloud.client.carts.add(this.configuration.get('cart_id'),[
-    //     line_item
-    // ])
-  	// .then((response) => {
-    //   loading.dismiss();
-    //   let alert = this.alertCtrl.create({
-    //       title: 'Added to cart!',
-    //       subTitle: 'Item added to cart',
-    //       buttons: ['Ok']
-    //     });
-
-    //     alert.present();
-
-    // })
-    // .catch((error) => {
-    //   loading.dismiss();
-    //   let alert = this.alertCtrl.create({
-    //       title: 'Error',
-    //       subTitle: 'An error has occurred, please retry.',
-    //       buttons: ['Ok']
-    //     });
-    //     alert.present();
-
-    // })
+  	console.log(this.itemsProvider.getCart());
   }
 }
--- src/providers/items/items.ts
+++ src/providers/items/items.ts
@@ -1,32 +1,47 @@
 import { Injectable } from '@angular/core';
 import 'rxjs/add/operator/map';
 
-/*
-  Generated class for the ItemsProvider provider.
-
-  See https://angular.io/docs/ts/latest/guide/dependency-injection.html
-  for more info on providers and Angular 2 DI.
-*/
 @Injectable()
 export class ItemsProvider {
     products = [{
       'src':'https://support.apple.com/content/dam/edam/applecare/images/en_US/iphone/featured-content-iphone-transfer-content-ios10_2x.png',
       'description': 'Patta phone eka',
       'name': 'iPhone 4S',
-      'price_discount': 55.0,
-      'price': 500
+      'price_discount': '55.0',
+      'price': '500',
+      'id': '01'
   },{
       'src':'https://store.storeimages.cdn-apple.com/4974/as-images.apple.com/is/image/AppleInc/aos/published/images/i/ph/iphone7/plus/iphone7-plus-black-select-2016?wid=1200&hei=630&fmt=jpeg&qlt=95&op_sharpen=0&resMode=bicub&op_usm=0.5,0.5,0,0&iccEmbed=0&layer=comp&.v=1472430090682',
       'description': 'Patta phone eka',
       'name': 'iPhone 4S',
-      'price_discount': 55.0,
-      'price': 500
+      'price_discount': '55.0',
+      'price': '500',
+      'id': '02'
   }];
 
-  cart = [];
+  cart = [{
+      'src':'https://store.storeimages.cdn-apple.com/4974/as-images.apple.com/is/image/AppleInc/aos/published/images/i/ph/iphone7/plus/iphone7-plus-black-select-2016?wid=1200&hei=630&fmt=jpeg&qlt=95&op_sharpen=0&resMode=bicub&op_usm=0.5,0.5,0,0&iccEmbed=0&layer=comp&.v=1472430090682',
+      'description': 'Patta phone eka',
+      'name': 'iPhone 4S',
+      'price_discount': '55.0',
+      'price': '500',
+      'id': '02'
+  }];
 
   getItems(){
     return this.products;
+  }
+
+  getCart(){
+    return this.cart;
+  }
+
+  addToCart(item){
+    this.cart.push(item);
+  }
+
+  clearCart(){
+    this.cart = [];
   }
 
   constructor() {
